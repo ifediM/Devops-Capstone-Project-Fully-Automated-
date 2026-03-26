16eff4e6e875738019689a44afb5d9840c30ad72
ifediM <kingsley.maduekwe1@gmail.com>
2026-03-26 02:57:41 +0000

    update

--- oidc/backend.tf
+++ oidc/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "devops-capstone"
-    key            = "backend/terraform.tfstate"
+    key            = "/demo/backend/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = true
