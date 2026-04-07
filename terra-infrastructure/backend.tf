4440f2d16ac6b0c3a05e0e05cad3aba2de54a17b
ifediM <kingsley.maduekwe1@gmail.com>
2026-04-07 23:42:26 +0100

    typo

--- terra-infrastructure/backend.tf
+++ terra-infrastructure/backend.tf
@@ -4,7 +4,7 @@ terraform {
     bucket = "devops-capstone-ifedi"
     key    = "demo/backend/terraform.tfstate"
     region = "us-east-1"
-    profile        = "capstone"
+    #profile        = "capstone"
     encrypt      = true
     use_lockfile = true
   }
